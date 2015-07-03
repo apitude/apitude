2e37b56f063093f4471d33ceec453637e662863e
Gordon <baohx2000@gmail.com>
2015-07-03 16:19:53 -0700

    cleanup

--- src/Provider/RedisServiceProvider.php
+++ src/Provider/RedisServiceProvider.php
@@ -6,12 +6,12 @@ use Silex\ServiceProviderInterface;
 
 class RedisServiceProvider implements ServiceProviderInterface
 {
-
     /**
      * Registers services on the given app.
      *
      * This method should only be used to configure services and parameters.
      * It should not get services.
+     * @param Application $app
      */
     public function register(Application $app)
     {
@@ -29,6 +29,7 @@ class RedisServiceProvider implements ServiceProviderInterface
      * This method is called after all services are registered
      * and should be used for "dynamic" configuration (whenever
      * a service must be requested).
+     * @param Application $app
      */
     public function boot(Application $app)
     {
