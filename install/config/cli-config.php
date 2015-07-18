56746e7c2dade9d42d6c4b2a257b198111c63c1f
Gordon <baohx2000@gmail.com>
2015-07-18 14:07:51 -0700

    bootstrap location

--- install/config/cli-config.php
+++ install/config/cli-config.php
@@ -1,7 +1,7 @@
 <?php
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
-$app = require_once APP_PATH . '/bootstrap.php';
+$app = require_once '../bootstrap.php';
 $em = $app['em'];
 
 return ConsoleRunner::createHelperSet($em);
