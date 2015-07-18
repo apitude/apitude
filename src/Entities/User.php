2157bb5a7ff1d4e032de0114eeb3602bee8dc610
Gordon <baohx2000@gmail.com>
2015-07-18 15:57:36 -0700

    proxies dir for install, User get/setters

--- src/Entities/User.php
+++ src/Entities/User.php
@@ -29,4 +29,52 @@ abstract class User
      * @ORM\Column(type="string", length=128, nullable=true)
      */
     private $password;
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
 }
