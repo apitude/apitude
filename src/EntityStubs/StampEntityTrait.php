fb4bc3ec8e22b743db4c7b35093022a84354d27b
Gordon <baohx2000@gmail.com>
2015-07-19 13:47:03 -0700

    add getters/setters to stamp trait

--- src/EntityStubs/StampEntityTrait.php
+++ src/EntityStubs/StampEntityTrait.php
@@ -8,8 +8,6 @@ use Doctrine\ORM\Mapping as ORM;
  * Class StampEntityTrait
  * @package EntityStubs
  * @ORM\Entity
- * @method setCreated(\DateTime $dateTime)
- * @method setModified(\DateTime $dateTime)
  */
 trait StampEntityTrait
 {
@@ -38,4 +36,76 @@ trait StampEntityTrait
      * @ORM\JoinColumn(name="modify_user_id", referencedColumnName="id")
      */
     private $modifiedBy;
+
+    /**
+     * @return User
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * @param User $createdBy
+     * @return StampEntityTrait
+     */
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+        return $this;
+    }
+
+    /**
+     * @return User
+     */
+    public function getModifiedBy()
+    {
+        return $this->modifiedBy;
+    }
+
+    /**
+     * @param User $modifiedBy
+     * @return StampEntityTrait
+     */
+    public function setModifiedBy($modifiedBy)
+    {
+        $this->modifiedBy = $modifiedBy;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * @param \DateTime $created
+     * @return StampEntityTrait
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getModified()
+    {
+        return $this->modified;
+    }
+
+    /**
+     * @param \DateTime $modified
+     * @return StampEntityTrait
+     */
+    public function setModified($modified)
+    {
+        $this->modified = $modified;
+        return $this;
+    }
 }
