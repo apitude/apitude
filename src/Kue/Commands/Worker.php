a1b51336116b9f6411c1ce1b37ac64f424cf7a58
Gordon Forsythe <baohx2000@gmail.com>
2015-10-28 10:51:47 -0700

    kue worker wip

--- src/Kue/Commands/Worker.php
+++ src/Kue/Commands/Worker.php
@@ -3,9 +3,10 @@ namespace Apitude\Core\Kue\Commands;
 
 use Apitude\Core\Commands\BaseCommand;
 use Apitude\Core\Kue\PreforkingWorker;
+use Kue\Job;
 use Kue\SequentialWorker;
 use Monolog\Handler\StreamHandler;
-use Psr\Log\LoggerInterface;
+use Monolog\Logger;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -14,9 +15,12 @@ use Symfony\Component\Console\Output\OutputInterface;
 class Worker extends BaseCommand
 {
     /**
-     * @var LoggerInterface
+     * @var Logger
      */
     private $log;
+
+    private $events = [];
+    private $queue;
 
     public function configure()
     {
@@ -26,33 +30,34 @@ class Worker extends BaseCommand
         $this->setName('kue:work')
             ->setDescription('Processes jobs put into the queue')
             ->addArgument('queue', InputArgument::OPTIONAL, 'Location key for queue', 'kue.queue')
+            ->addOption('qname', 'q', InputOption::VALUE_REQUIRED, 'Queue name to process', 'kue:default')
             ->addOption('workers', 'c', InputOption::VALUE_REQUIRED, 'Number of workers', 1)
             ->addOption('require', 'r', InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'File(s) to require before accepting jobs');
 
         parent::configure();
     }
 
-    protected function setupWorker(Worker $worker)
+    protected function setupWorker(\Kue\Worker $worker)
     {
         $log = $this->log;
 
         $worker->on('init', function(Job $job) use ($log) {
-            $log->addInfo(sprintf('Accepted job %s', get_class($job)), array('job' => $job));
+            $log->info(sprintf('Accepted job %s', get_class($job)), array('job' => $job));
         });
 
         $worker->on('error', function(Job $job, $code, $message, $file, $line) use ($log) {
-            $log->addError(
+            $log->error(
                 sprintf('Job %s failed: "%s" in file %s:%d', get_class($job), $message, $file, $line),
                 array('job' => $job)
             );
         });
 
         $worker->on('exception', function(Job $job, \Exception $exception) use ($log) {
-            $log->addError(sprintf('Job "%s" failed: %s', get_class($job), $exception), array('job' => $job));
+            $log->error(sprintf('Job "%s" failed: %s', get_class($job), $exception), array('job' => $job));
         });
 
         $worker->on('success', function(Job $job) use ($log) {
-            $log->addInfo(sprintf('Job "%s" finished successfully', get_class($job)), array('job' => $job));
+            $log->info(sprintf('Job "%s" finished successfully', get_class($job)), array('job' => $job));
         });
 
         foreach ($this->events as $event => $handler) {
